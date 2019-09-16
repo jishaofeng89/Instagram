14fc947215ab1decd69c340fbc4cb3428aaecd20
jishaofeng89 <1195957057@qq.com>
2019-09-16 22:04:29 +0800

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Administrator on 2019/9/16.
+//  Copyright © 2019 smecta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
